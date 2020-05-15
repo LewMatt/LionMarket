b0d0eabfb11c1ec3e2005245b07a630080a1359c
LewMatt <lewmateush@gmail.com>
2020-05-15 23:26:58 +0200

    Project created.

---
+++ LionMarketApp/LionMarketApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LionMarketApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            FormMenu fMenu = new FormMenu();
+            this.Hide();
+            fMenu.Show();
+        }
+    }
+}
