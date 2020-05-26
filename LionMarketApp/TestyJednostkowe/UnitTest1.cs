0c522e57c6c3e007819aed32b34bd6e5572a150c
LewMatt <lewmateush@gmail.com>
2020-05-26 17:56:25 +0200

    Files updated.

--- LionMarketApp/TestyJednostkowe/UnitTest1.cs
+++ LionMarketApp/TestyJednostkowe/UnitTest1.cs
@@ -14,12 +14,13 @@ namespace TestyJednostkowe
             //objekt testowy
             UserControl1Pieczywo pieczObj = new UserControl1Pieczywo();
 
+            //tworzymy kontrolke na potrzeby testów jednostkowych
             pieczObj.listViewPieczywo.CreateControl();
 
             //cena produktu
             double cena = 5;
 
-            //ile wybranych produktów
+            //ilość sztuk wybranego produktu
             int ilosc = 3;
 
             //ustawienie ilosci w polu wyboru
@@ -31,6 +32,7 @@ namespace TestyJednostkowe
             //suma oczekiwana
             double suma_o = cena * ilosc;
 
+            //sprawdzanie danych
             Assert.AreEqual(suma_o, pieczObj.suma, 0.001, "Nie działa");
 
         }
@@ -38,46 +40,62 @@ namespace TestyJednostkowe
         [TestMethod]
         public void TestMethod2()
         {
+            //objekt testowy
             UserControlNabial nabObj = new UserControlNabial();
 
+            //tworzymy kontrolke na potrzeby testów jednostkowych
             nabObj.listViewNabial.CreateControl();
 
+            //cena produktu
             double cena = 20;
 
+            //ilość wybranych produktów
             int ilosc = 4;
 
+            //ilość jednorazowego dodania 
             int x = 1;
 
+            //dodanie produktów do koszyka
             for(int i = 0;i<ilosc;i++)
             {
                 nabObj.textBoxIleSztuk.Text = x.ToString();
                 nabObj.dodajDoKoszyka();
             }
 
+            //suma oczekiwana
             double suma_o = cena * ilosc;
 
+            //sprawdzenie danych
             Assert.AreEqual(suma_o, nabObj.suma, 0.001, "Nie działa");
+
         }
 
         [TestMethod]
         public void TestMethod3()
         {
+            //objekt testowy
             UserControlSlodycze slodObj = new UserControlSlodycze();
 
+            //tworzymy kontrolke na potrzeby testów jednostkowych
             slodObj.listViewSlodycze.CreateControl();
 
-            double ilosc = 27;
+            //ilosc produktów do dodania
+            double ilosc = 14;
 
+            //ilość sztuk w pojedynczym dodaniu
             int x = 1;
 
+            //dodanie produktów do koszyka
             for(int i = 0;i<ilosc;i++)
             {
                 slodObj.textBoxIleSztuk.Text = x.ToString();
                 slodObj.dodajDoKoszyka();
             }
 
+            //ilość produktów dodanych do koszyka
             int ilosc_prod_dodanych = slodObj.wybraneProdukty.Count;
 
+            //sprawdzenie danych
             Assert.AreEqual(ilosc, ilosc_prod_dodanych, 0.001, "Nie działa");
 
         }
